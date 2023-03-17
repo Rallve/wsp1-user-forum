2b231ad81dc1b2fa74e2452131b8b9a3389618e0
Rallve <lucas.liedgren@gmail.com>
2023-03-17 11:12:13 +0100

    gjorde grejer

---
+++ logg.md
@@ -0,0 +1,1 @@
+03/17 - Började koda
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,19 @@
 const express = require('express');
 const router = express.Router();
+const mysql = require('mysql2');
+const pool = mysql.createPool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    database: process.env.DB_DATABASE,
+    password: process.env.DB_PASSWORD,
+});
+const promisePool = pool.promise();
+
 /* GET home page. */
 router.get('/', function (req, res) {
     return res.json({ msg: 'Hello World' });
 });
+
+
 
 module.exports = router;
