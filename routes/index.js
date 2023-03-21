08d0606b4d4903e9b7c5a267bd1c30bbf4624e3f
Rallve <lucas.liedgren@gmail.com>
2023-03-21 14:51:41 +0100

    borde kanske ha delat upp mina commits

--- routes/index.js
+++ routes/index.js
@@ -10,10 +10,19 @@ const pool = mysql.createPool({
 const promisePool = pool.promise();
 
 /* GET home page. */
-router.get('/', function (req, res) {
+/*
+router.get('/', async function (req, res) {
     return res.json({ msg: 'Hello World' });
 });
-
+*/
+router.get('/', async function (req, res, next) {
+    const [rows] = await promisePool.query("SELECT * FROM lgl23forum");
+    res.render('index.njk', {
+        rows: rows,
+        title: 'Forum',
+        //nav: nav
+    });
+});
 
 
 module.exports = router;
